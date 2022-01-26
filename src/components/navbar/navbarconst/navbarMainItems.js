4ec9c3934aada2658081eddd059f98083f835b02
cucuy555 <cucuycy555@gmail.com>
2022-01-26 22:56:47 +0200

    #2

---
+++ src/components/navbar/navbarconst/navbarMainItems.js
@@ -0,0 +1,38 @@
+export const mainNavbarItems = [
+  {
+    id: 0,
+    icon: <PeopleIcon />,
+    label: "Authentication",
+    route: "authentication",
+  },
+  {
+    id: 1,
+    icon: <DnsIcon />,
+    label: "Database",
+    route: "database",
+  },
+  {
+    id: 2,
+    icon: <ImageIcon />,
+    label: "Storage",
+    route: "storage",
+  },
+  {
+    id: 3,
+    icon: <PublicIcon />,
+    label: "Hosting",
+    route: "hosting",
+  },
+  {
+    id: 4,
+    icon: <SettingsEthernetIcon />,
+    label: "Functions",
+    route: "functions",
+  },
+  {
+    id: 5,
+    icon: <SettingsInputComponentIcon />,
+    label: "Machine learning",
+    route: "machine-learning",
+  },
+];
