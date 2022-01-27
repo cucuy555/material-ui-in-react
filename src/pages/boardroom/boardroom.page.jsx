743e4e69d4cf46cbc467583c39c4964539c07af1
Z3u5 <theodorou.savvas@gmail.com>
2022-01-27 17:12:12 +0200

    4 minutes

--- src/pages/boardroom/boardroom.page.jsx
+++ src/pages/boardroom/boardroom.page.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import { CommonButton } from "../../components/common/commonButton/commonButton";
 
 function Boardroom() {
+  const buttonStyles = {
+    fontSize: 20,
+    fontWeight: 500,
+    backgroundColor: "red",
+    "&:hover": {
+      backgroundColor: "yellow",
+    },
+  };
   return (
     <Grid>
       <Grid item xs={8}>
         <h1>Boardroom page</h1>
+        <CommonButton variant="contained" size="large" sx={buttonStyles}>
+          Button
+        </CommonButton>
       </Grid>
     </Grid>
   );
